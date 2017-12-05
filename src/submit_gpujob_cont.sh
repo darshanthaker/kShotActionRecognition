f839360cbdfe829bde00398441c69e2e8c736e70
krishnakumar-kapil <krishnakumar.kapil@utexas.edu>
2017-12-05 01:06:28 -0600

    scripts

--- src/submit_gpujob_cont.sh
+++ src/submit_gpujob_cont.sh
@@ -22,8 +22,8 @@
 # http://www.tacc.utexas.edu/user-services/user-guides/maverick-user-guide
 #-----------------------------------------------------------------------------
 #
-#SBATCH -J al_med                           # Job name
-#SBATCH -o job_outputs/difficulty/al_med-%j.out                     # Name of stdout output file (%j expands to jobId)
+#SBATCH -J al_pretrained                           # Job name
+#SBATCH -o job_outputs/controller/al_pretrained-%j.out                     # Name of stdout output file (%j expands to jobId)
 #SBATCH -p gpu                                      # Queue name
 #SBATCH -N 1                                        # Total number of nodes requested (20 cores/node)
 #SBATCH -n 1                                        # Total number of mpi tasks requested
@@ -35,4 +35,4 @@
 module load cuda/8.0 cudnn/5.1
 module load tensorflow-gpu
 # TODO: REMEMBER TO CHANGE JOB NAME
-python3 one_shot_learning.py --dataset_type=kinetics_dynamic --controller_type=alex --batch_size=16 --image_width=128  --image_height=128 --summary_writer=True --model_saver=False --debug=True --memory_size=128 --memory_vector_dim=40 --seq_length=100 --n_classes=25 --class_difficulty=medium --use_pretrained=False --num_epoches=1000 --rnn_size=200 --batches_validation=5
+python3 one_shot_learning.py --dataset_type=kinetics_dynamic --controller_type=alex --batch_size=16 --image_width=128  --image_height=128 --summary_writer=True --model_saver=False --debug=True --memory_size=128 --memory_vector_dim=40 --seq_length=100 --n_classes=25 --class_difficulty=all --use_pretrained=True --num_epoches=1000 --rnn_size=200 --batches_validation=5
