999a93dba30e7330860159337285c8dc1b1ad7bc
krishnakumar-kapil <krishnakumar.kapil@utexas.edu>
2017-12-04 19:02:52 -0600

    updated splits to have test and train

--- src/split_data.py
+++ src/split_data.py
@@ -21,10 +21,15 @@ medium_index = accuracies.index(MEDIUM)
 hard_class_list = np.array([data_tuples[i][0] for i in range(hard_index)])
 medium_class_list = np.array([data_tuples[i][0] for i in range(hard_index, medium_index)])
 easy_class_list = np.array([data_tuples[i][0] for i in range(medium_index, len(data_tuples))])
-set_trace()
 
-np.save('hard.npy', hard_class_list)
-np.save('medium.npy', medium_class_list)
-np.save('easy.npy', easy_class_list)
+def save_split(class_list, name):
+    np.random.shuffle(class_list)
+    train_split = class_list[:len(class_list)//2]
+    test_split = class_list[len(class_list)//2:]
+    set_trace()
+    np.save("{}_train.npy".format(name), train_split)
+    np.save("{}_test.npy".format(name), test_split)
 
-
+save_split(hard_class_list, 'hard')
+save_split(medium_class_list, 'medium')
+save_split(easy_class_list, 'easy')
--- src/submit_gpujob.sh
+++ src/submit_gpujob.sh
@@ -22,8 +22,8 @@
 # http://www.tacc.utexas.edu/user-services/user-guides/maverick-user-guide
 #-----------------------------------------------------------------------------
 #
-#SBATCH -J rnn300                           # Job name
-#SBATCH -o job_outputs/memory/rnn300-%j.out                     # Name of stdout output file (%j expands to jobId)
+#SBATCH -J classes25                           # Job name
+#SBATCH -o job_outputs/classes/classes25-%j.out                     # Name of stdout output file (%j expands to jobId)
 #SBATCH -p gpu                                      # Queue name
 #SBATCH -N 1                                        # Total number of nodes requested (20 cores/node)
 #SBATCH -n 1                                        # Total number of mpi tasks requested
@@ -35,4 +35,4 @@
 module load cuda/8.0 cudnn/5.1
 module load tensorflow-gpu
 # TODO: REMEMBER TO CHANGE JOB NAME
-python3 one_shot_learning.py --dataset_type=kinetics_dynamic --controller_type=alex --batch_size=16 --image_width=128  --image_height=128 --seq_length=35 --summary_writer=True --model_saver=False --debug=True --memory_size=128 --memory_vector_dim=40 --n_classes=5 --class_difficulty=easy --use_pretrained=True --num_epoches=1000 --rnn_size=300
+python3 one_shot_learning.py --dataset_type=kinetics_dynamic --controller_type=alex --batch_size=16 --image_width=128  --image_height=128 --seq_length=100 --summary_writer=True --model_saver=False --debug=True --memory_size=128 --memory_vector_dim=40 --n_classes=25 --class_difficulty=easy --use_pretrained=False --num_epoches=1000 --rnn_size=200
