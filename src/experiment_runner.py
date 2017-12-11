a2b1c816430c221dcbf283f8424129896cf01c36
krishnakumar-kapil <krishnakumar.kapil@utexas.edu>
2017-12-11 01:51:30 -0600

    exp runner

--- src/experiment_runner.py
+++ src/experiment_runner.py
@@ -6,7 +6,7 @@ import subprocess
 from pdb import set_trace
 from util import eprint
 
-VALID_N_CLASSES = [5, 15, 25]
+VALID_N_CLASSES = [5, 10, 15, 25]
 VALID_CONTROLLERS = ['alex', 'vgg19', 'default']
 VALID_CLASS_DIFFICULTIES = ['easy', 'medium', 'hard', 'all']
 
@@ -53,16 +53,18 @@ exp_to_folder_map = {'al_med': 'difficulty', 'al_hard': 'difficulty','al_easy': 
                             'mem256x40':'memory','mem32x40':'memory','mem16x40':'memory',\
                             'mem16x20':'memory',
                     'center_frame': 'inputs',
+                    'n10seq70': 'classes','n15seq105': 'classes','n25seq175': 'classes',
                     'omni_alex': 'omniglot',
                     'omni_default': 'omniglot',
                     'lstm100': 'lstm', 'lstm1': 'lstm', 'lstm300':'lstm', 'lstm25':'lstm',
                     'default_controller': 'controllers', 'alex_pretrained':"controllers", 'vgg': 'controllers',\
-                            'alex_not_pretrained': 'controllers'} 
+                            'alex_not_pretrained': 'controllers',
+                    'uniform':'sampling'} 
 
 all_configs = {
-               #  'controllers/vgg': ExperimentConfig(controller_type='vgg19', \
-                                                   #  image_width=64, image_height=64, \
-                                                   #  batch_size=8),
+               'controllers/vgg': ExperimentConfig(controller_type='vgg19', \
+                                                   image_width=64, image_height=64, \
+                                                   batch_size=8),
                #  'controllers/default_controller': ExperimentConfig(controller_type='default', \
                                                    #  image_width=64, image_height=64),
                #  'controllers/alex_pretrained': ExperimentConfig(controller_type='alex',use_pretrained=True),
@@ -70,28 +72,30 @@ all_configs = {
                #  'difficulty/al_med': ExperimentConfig(class_difficulty='medium'), \
                #  'difficulty/al_hard': ExperimentConfig(class_difficulty='hard'), \
                #  'difficulty/al_easy': ExperimentConfig(class_difficulty='easy') \
-               'no_norm/no_norm': ExperimentConfig(im_normalization=False),
-               'no_norm/norm': ExperimentConfig(im_normalization=True),
-               'memory/mem128x20': ExperimentConfig(memory_size=128, memory_vector_dim=20),
-               'memory/mem128x80': ExperimentConfig(memory_size=128, memory_vector_dim=80),
-               'memory/mem128x320': ExperimentConfig(memory_size=128, memory_vector_dim=320),
-               'memory/mem64x40': ExperimentConfig(memory_size=64, memory_vector_dim=40),
-               'memory/mem256x40': ExperimentConfig(memory_size=256, memory_vector_dim=40),
-               'memory/mem2x40': ExperimentConfig(memory_size=2, memory_vector_dim=40),
-               'memory/mem32x40': ExperimentConfig(memory_size=32, memory_vector_dim=40),
-               'memory/mem16x40': ExperimentConfig(memory_size=16, memory_vector_dim=40),
-               'memory/mem16x20': ExperimentConfig(memory_size=16, memory_vector_dim=20),
-               'inputs/center_frame': ExperimentConfig(dataset_type='kinetics_single_frame', seq_length=35, n_classes=5, num_epoches=250, batches_validation=1),
-               'lstm/lstm1': ExperimentConfig(rnn_size=1),
-               'lstm/lstm25': ExperimentConfig(rnn_size=25),
-               'lstm/lstm100': ExperimentConfig(rnn_size=100),
-               'lstm/lstm300': ExperimentConfig(rnn_size=300),
-               'seq_length/seq50': ExperimentConfig(seq_length=50),
-               'seq_length/seq150': ExperimentConfig(seq_length=150),
-               'seq_length/seq200': ExperimentConfig(seq_length=150),
-               'omniglot/omni_alex': ExperimentConfig(dataset_type='omniglot', controller_type='alex', image_height=20, image_width=20, n_classes=5, seq_length=35, num_epoches=17000),
-               'omniglot/omni_default': ExperimentConfig(dataset_type='omniglot', controller_type='default', image_height=20, image_width=20, n_classes=5, seq_length=35, num_epoches=17000),
-               #  'sampling'
+               #  'no_norm/no_norm': ExperimentConfig(im_normalization=False),
+               #  'no_norm/norm': ExperimentConfig(im_normalization=True),
+               #  'memory/mem128x20': ExperimentConfig(memory_size=128, memory_vector_dim=20),
+               #  'memory/mem128x80': ExperimentConfig(memory_size=128, memory_vector_dim=80),
+               #  'memory/mem128x320': ExperimentConfig(memory_size=128, memory_vector_dim=320),
+               #  'memory/mem64x40': ExperimentConfig(memory_size=64, memory_vector_dim=40),
+               #  'memory/mem256x40': ExperimentConfig(memory_size=256, memory_vector_dim=40),
+               #  'memory/mem2x40': ExperimentConfig(memory_size=2, memory_vector_dim=40),
+               #  'memory/mem32x40': ExperimentConfig(memory_size=32, memory_vector_dim=40),
+               #  'memory/mem16x40': ExperimentConfig(memory_size=16, memory_vector_dim=40),
+               #  'memory/mem16x20': ExperimentConfig(memory_size=16, memory_vector_dim=20),
+               #  'inputs/center_frame': ExperimentConfig(dataset_type='kinetics_single_frame', seq_length=35, n_classes=5, num_epoches=250, batches_validation=1),
+               #  'lstm/lstm1': ExperimentConfig(rnn_size=1),
+               #  'lstm/lstm25': ExperimentConfig(rnn_size=25),
+               #  'lstm/lstm100': ExperimentConfig(rnn_size=100),
+               #  'lstm/lstm300': ExperimentConfig(rnn_size=300),
+               #  'classes/n10seq70': ExperimentConfig(n_classes=10, seq_length=70),
+               #  'classes/n15seq105': ExperimentConfig(n_classes=15, seq_length=105),
+               #  'classes/n25seq175': ExperimentConfig(n_classes=25, seq_length=175)
+               #  'omniglot/omni_alex': ExperimentConfig(dataset_type='omniglot', controller_type='alex', image_height=20, image_width=20, n_classes=5, seq_length=35, num_epoches=17000),
+
+               #  'omniglot/omni_default': ExperimentConfig(dataset_type='omniglot', controller_type='default', image_height=20, image_width=20, n_classes=5, seq_length=35, num_epoches=17000),
+                  'sampling/uniform': ExperimentConfig(sampling_strategy='uniform'),
+                  'no_norm/no_norm': ExperimentConfig(im_normalization=False),
               }
 
 class ExperimentRunner(object):
