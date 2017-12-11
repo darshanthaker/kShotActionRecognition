bbfe92593d3a534ecfabf66d01b617ab9958a64c
Darshan Thaker <dbthaker@gmail.com>
2017-12-10 19:13:36 -0600

    merge

--- src/experiment_runner.py
+++ src/experiment_runner.py
@@ -51,7 +51,7 @@ exp_to_folder_map = {'al_med': 'difficulty', 'al_hard': 'difficulty','al_easy': 
                     'no_norm': 'no_norm',
                     'mem128x80': 'memory', 'mem128x20': 'memory', 'mem64x40':'memory', 'mem2x40':'memory',\
                             'mem256x40':'memory','mem32x40':'memory','mem16x40':'memory',\
-                            'mem16x20':'memory',
+                            'mem16x20':'memory', 'mem128x320': 'memory', \
                     'center_frame': 'inputs',
                     'omni_alex': 'omniglot',
                     'omni_default': 'omniglot',
@@ -60,18 +60,18 @@ exp_to_folder_map = {'al_med': 'difficulty', 'al_hard': 'difficulty','al_easy': 
                             'alex_not_pretrained': 'controllers'} 
 
 all_configs = {
-               #  'controllers/vgg': ExperimentConfig(controller_type='vgg19', \
-                                                   #  image_width=64, image_height=64, \
-                                                   #  batch_size=8),
-               #  'controllers/default_controller': ExperimentConfig(controller_type='default', \
-                                                   #  image_width=64, image_height=64),
-               #  'controllers/alex_pretrained': ExperimentConfig(controller_type='alex',use_pretrained=True),
-               #  'controllers/alex_not_pretrained': ExperimentConfig(controller_type='alex'),
-               #  'difficulty/al_med': ExperimentConfig(class_difficulty='medium'), \
-               #  'difficulty/al_hard': ExperimentConfig(class_difficulty='hard'), \
-               #  'difficulty/al_easy': ExperimentConfig(class_difficulty='easy') \
-               'no_norm/no_norm': ExperimentConfig(im_normalization=False),
-               'no_norm/norm': ExperimentConfig(im_normalization=True),
+               # 'controllers/vgg': ExperimentConfig(controller_type='vgg19', \
+                                                   # image_width=64, image_height=64, \
+                                                   # batch_size=8),
+               # 'controllers/default_controller': ExperimentConfig(controller_type='default', \
+                                                   # image_width=64, image_height=64),
+               # 'controllers/alex_pretrained': ExperimentConfig(controller_type='alex',use_pretrained=True),
+               # 'controllers/alex_not_pretrained': ExperimentConfig(controller_type='alex'),
+               # 'difficulty/al_med': ExperimentConfig(class_difficulty='medium'), \
+               # 'difficulty/al_hard': ExperimentConfig(class_difficulty='hard'), \
+               # 'difficulty/al_easy': ExperimentConfig(class_difficulty='easy') \
+               #  'no_norm/no_norm': ExperimentConfig(im_normalization=False),
+               #  'no_norm/norm': ExperimentConfig(im_normalization=True),
                'memory/mem128x20': ExperimentConfig(memory_size=128, memory_vector_dim=20),
                'memory/mem128x80': ExperimentConfig(memory_size=128, memory_vector_dim=80),
                'memory/mem128x320': ExperimentConfig(memory_size=128, memory_vector_dim=320),
@@ -90,8 +90,7 @@ all_configs = {
                'seq_length/seq150': ExperimentConfig(seq_length=150),
                'seq_length/seq200': ExperimentConfig(seq_length=150),
                'omniglot/omni_alex': ExperimentConfig(dataset_type='omniglot', controller_type='alex', image_height=20, image_width=20, n_classes=5, seq_length=35, num_epoches=17000),
-               'omniglot/omni_default': ExperimentConfig(dataset_type='omniglot', controller_type='default', image_height=20, image_width=20, n_classes=5, seq_length=35, num_epoches=17000),
-               #  'sampling'
+               'omniglot/omni_default': ExperimentConfig(dataset_type='omniglot', controller_type='default', image_height=20, image_width=20, n_classes=5, seq_length=35, num_epoches=17000)
               }
 
 class ExperimentRunner(object):
